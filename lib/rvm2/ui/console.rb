6c33fbd3182d04680755cbdab619bd873f081a3f
Michal Papis <mpapis@gmail.com>
2013-12-03 15:19:06 +0100

    add the missing Console class

--- lib/rvm2/ui/console.rb
+++ lib/rvm2/ui/console.rb
@@ -1,10 +1,12 @@
 module Rvm2
   module Ui
-    def start(name)
-    end
-    def finish(name)
-    end
-    def log(message, type = :log)
+    class Console
+      def start(name)
+      end
+      def finish(name)
+      end
+      def log(message, type = :log)
+      end
     end
   end
 end
