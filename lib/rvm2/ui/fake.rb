fcb57f597392dd79b6f6b135cccd29533213ccba
Michal Papis <mpapis@gmail.com>
2013-12-04 07:05:28 +0100

    add fake handler scaffold

---
+++ lib/rvm2/ui/fake.rb
@@ -0,0 +1,30 @@
+module Rvm2
+  module Ui
+    class Fake
+      class Element
+        attr_reader :list, :message, :type, :parent
+        def initialize(message, type = :log, parent = nil)
+          @message = message
+          @type    = type
+          @parent  = parent
+          @list    = [] unless parent.nil?
+        end
+      end
+      def initialize
+        root = Element.new(nil, :group, nil)
+        current = root
+      end
+      def start(name)
+        created = Element.new(name, :group, current)
+        current.list << created
+        current =  created
+      end
+      def finish
+        current = current.parent
+      end
+      def log(message, type = :log)
+        current << Element.new(message, type)
+      end
+    end
+  end
+end
