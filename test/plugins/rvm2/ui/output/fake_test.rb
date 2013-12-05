af39bdf7f6cb530e9f5c241bb2615d5d9e60cc3b
Michal Papis <mpapis@gmail.com>
2013-12-05 12:08:54 +0100

    add test for creating flat groups in fake

--- test/plugins/rvm2/ui/output/fake_test.rb
+++ test/plugins/rvm2/ui/output/fake_test.rb
@@ -13,4 +13,16 @@ describe Rvm2::Ui::Output::Fake do
     subject.root.list.size.must_equal(2)
     subject.root.list.map(&:message).must_equal(["Test me 1", "Test me 2"])
   end
+
+  it "adds groups" do
+    subject.root.list.must_equal([])
+    subject.start("Group 1")
+    subject.finish(true)
+    subject.root.list.size.must_equal(1)
+    subject.start("Group 2")
+    subject.finish(false)
+    subject.root.list.size.must_equal(2)
+    subject.root.list.map(&:message).must_equal(["Group 1", "Group 2"])
+    subject.root.list.map(&:status).must_equal([true, false])
+  end
 end
