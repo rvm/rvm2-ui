2c907ecbaa9c39b857d14cc828f8ec8bb1b83cad
Michal Papis <mpapis@gmail.com>
2013-12-05 11:03:24 +0100

    current and root need to be instance variables

--- lib/rvm2/ui/fake.rb
+++ lib/rvm2/ui/fake.rb
@@ -1,6 +1,7 @@
 module Rvm2
   module Ui
     class Fake
+
       class Element
         attr_reader :list, :message, :type, :parent
         def initialize(message, type = :log, parent = nil)
@@ -10,21 +11,27 @@ module Rvm2
           @list    = [] unless parent.nil?
         end
       end
+
+      attr_reader :root
       def initialize
-        root = Element.new(nil, :group, nil)
-        current = root
+        @root = Element.new(nil, :group, nil)
+        @current = @root
       end
+
       def start(name)
-        created = Element.new(name, :group, current)
-        current.list << created
-        current =  created
+        created = Element.new(name, :group, @current)
+        @current.list << created
+        @current =  created
       end
+
       def finish
-        current = current.parent
+        @current = @current.parent
       end
+
       def log(message, type = :log)
-        current << Element.new(message, type)
+        @current << Element.new(message, type)
       end
+
     end
   end
 end
