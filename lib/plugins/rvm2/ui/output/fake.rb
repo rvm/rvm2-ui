615222557f87bdd8e192d617b9a17472d1d3ccf2
Michal Papis <mpapis@gmail.com>
2013-12-05 21:22:14 +0100

    add stdout and stderr to fake output

--- lib/plugins/rvm2/ui/output/fake.rb
+++ lib/plugins/rvm2/ui/output/fake.rb
@@ -1,3 +1,5 @@
+require 'stringio'
+
 module Rvm2
   module Ui
     module Output
@@ -14,7 +16,18 @@ module Rvm2
           end
         end
 
-        attr_reader :root
+        class StringElements < StringIO
+          def initialize(parent, type)
+            @parent = parent
+            @type = type
+            super("","w")
+          end
+          def write(string)
+            @parent.current.list << Element.new(string, @type)
+          end
+        end
+
+        attr_reader :root, :current
         def initialize
           @root = Element.new(nil, :group, nil)
           @current = @root
@@ -33,6 +46,14 @@ module Rvm2
 
         def log(message, type = :log)
           @current.list << Element.new(message, type)
+        end
+
+        def stdout
+          @stdout ||= StringElements.new(self, :stdout)
+        end
+
+        def stderr
+          @stderr ||= StringElements.new(self, :stderr)
         end
 
       end
--- test/plugins/rvm2/ui/output/fake_test.rb
+++ test/plugins/rvm2/ui/output/fake_test.rb
@@ -5,6 +5,7 @@ describe Rvm2::Ui::Output::Fake do
   subject do
     Rvm2::Ui::Output::Fake.new
   end
+
   it "adds messages" do
     subject.root.list.must_equal([])
     subject.log("Test me 1")
@@ -40,6 +41,27 @@ describe Rvm2::Ui::Output::Fake do
     subject.root.list[0].list[0].message.must_equal("Test me 1")
     subject.root.list[0].list[1].message.must_equal("Group 2")
     subject.root.list[0].list[1].status.must_equal(false)
+  end
+
+  it "adds stdout writes" do
+    subject.stdout.write("Example 1")
+    subject.root.list.size.must_equal(1)
+    subject.root.list[0].message.must_equal("Example 1")
+  end
+
+  it "adds groups stdout writes" do
+    subject.start("Group 1")
+    subject.stdout.write("Example 1")
+    subject.finish(true)
+    subject.stderr.write("Example 1")
+    subject.root.list.size.must_equal(2)
+    subject.root.list[0].message.must_equal("Group 1")
+    subject.root.list[0].status.must_equal(true)
+    subject.root.list[0].list.size.must_equal(1)
+    subject.root.list[0].list[0].message.must_equal("Example 1")
+    subject.root.list[0].list[0].type.must_equal(:stdout)
+    subject.root.list[1].message.must_equal("Example 1")
+    subject.root.list[1].type.must_equal(:stderr)
   end
 
 end
