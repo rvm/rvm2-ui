316b43d50a42b5613ca5bf0f20baa9f7fa4be428
Michal Papis <mpapis@gmail.com>
2013-12-14 10:05:08 +0100

    add log output and it's tests

---
+++ lib/plugins/rvm2/ui/output/log.rb
@@ -0,0 +1,17 @@
+module Rvm2
+  module Ui
+    module Output
+      class Log < Console
+        attr_reader :file_name
+        def initialize(file_name, flags = "w")
+          @file_name = file_name
+          @file = File.new(@file_name, flags)
+          super(@file, @file)
+        end
+        def finalize
+          @file.close
+        end
+      end
+    end
+  end
+end
---
+++ test/plugins_test/rvm2/ui/output/log_test.rb
@@ -0,0 +1,26 @@
+require 'test_helper'
+require 'plugins/rvm2/ui/output/log'
+require 'tempfile'
+
+describe Rvm2::Ui::Output::Log do
+  before do
+    @tempfile = Tempfile.new("rvm2-ui-output-log-test")
+    @tempfile.close
+  end
+  after do
+    @tempfile.unlink
+  end
+  subject do
+    Rvm2::Ui::Output::Log.new(@tempfile.path)
+  end
+
+  it "adds messages" do
+    subject.log("Test me 1")
+    subject.finalize
+    File.open(@tempfile.path, "r") do |file|
+      file.read.must_equal(<<-EXPECTED)
+Test me 1
+      EXPECTED
+    end
+  end
+end
